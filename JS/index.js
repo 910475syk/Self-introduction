2c993a086c072e27fcfae5caad933668d9198ae0
910475syk <26384652+910475syk@users.noreply.github.com>
2023-05-21 22:41:10 +0800

    Revies Self-introduction

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,77 @@
-$(document).ready(function(){
+$(document).ready(function () {
     //
 
+    // scroll
+    $(window).scroll(function () {
+        let scrollTop = $(document).scrollTop();
+        let about_height = $('.about').offset().top - $('.about').height() / 2;
+        let services_height = $('#personality').offset().top - $('#personality').height() * 2;
+
+        if (scrollTop > about_height) {
+            $('.about').addClass('enter_anime_after');
+        }
+        if (scrollTop > services_height) {
+            $('#personality').addClass('enter_anime_after');
+        }
+    })
+    $('a[href^=\\#]').click(function () {
+        var speed = 500;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+
+    // banner word
+    ityped.init(document.querySelector("#ityped"), {
+        showCursor: false,
+        backDelay: 2500,
+        strings: ['前端工程師志願', '期待與您合作']
+    })
+
     // btn
-    $('.btn').hover(function(){
+    $('.btn').hover(function () {
         $(this).toggleClass('penetrate');
     })
 
+    // slick
+    $('.slick').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 1000,
+        adaptiveHeight: false,
+        pauseOnFocus: false,
+        pauseOnHover: false
+    });
+
+    // popup
+    $('.brandListBtn').click(function () {
+        $('.popupBg').fadeIn();
+    });
+
+    $('.popup').on('click', function (event) {
+        event.stopPropagation();
+    })
+
+    $('.popup .popup_closeBtn').click(function () {
+        $('.popupBg').fadeOut();
+    })
+
+    $('.popupBg').on('click', function () {
+        $('.popupBg').fadeOut();
+    })
+
+
     //
+})
+
+$(window).on('load', function () {
+    $('#header').addClass('enter_anime_after');
+    $('.banner').addClass('enter_anime_after');
 })
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # 簡介
-這是一份關於我的自我介紹，其中參考了[億瑄數位行銷](https://yihsuanwebdesign.com/)公司的網頁進行設計，插圖的來源則是借用手機遊戲「明日方舟」的背景圖片
+這是一份關於我的自我介紹，請多多指教!
 
 # 實際呈現
 
@@ -7,4 +7,7 @@ URL: https://910475syk.github.io/Self-introduction/
 
 # Dependency
 * [JQuery](https://jquery.com/) 3.6.0
+* [font-awesome](https://fontawesome.com/)
 * [Reset CSS](https://meyerweb.com/eric/tools/css/reset/)
+* [slick](https://kenwheeler.github.io/slick/)
+* [ityped](https://unpkg.com/ityped@1.0.2)
